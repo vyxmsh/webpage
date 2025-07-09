3ed54ebf3a6299fd8f5c12ca89e7d73df68df8e0
vyxmsh <vyomeshnaithani@gmail.com>
2025-07-09 15:34:58 +0530

    Add files via upload

---
+++ httpserver.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) =>{
+    console.log(req.url)
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/html')
+    res.end('<h1>This is Bunny</h1> <br/> <p>Rock your body come on come on Rock your body</p>');
+
+})
+server.listen(port, ()=>{ console.log(`Server is listening on port ${port}`);});
