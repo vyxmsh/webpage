872d812e97bc6a89e7d8c4a43b84feeb1662e3fa
Sanya Gilhotra <sanyagilhotra@Sanyas-Laptop.local>
2025-07-16 22:22:19 +0530

    Added /hello page

--- httpserver.js
+++ httpserver.js
@@ -6,7 +6,11 @@ const server = http.createServer((req, res) =>{
     console.log(req.url)
     res.statusCode = 200;
     res.setHeader('Content-Type','text/html')
-    res.end('<h1>This is Bunny</h1> <br/> <p>Rock your body come on come on Rock your body</p>');
+       if (req.url === '/hello') {
+        res.end('<h1>Hello Page</h1><p>You visited /hello</p>');
+    } else {
+        res.end('<h1>This is Bunny</h1> <br/> <p>Rock your body come on come on Rock your body</p>');
+    }
+});
 
-})
 server.listen(port, ()=>{ console.log(`Server is listening on port ${port}`);});
